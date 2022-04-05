f3ac2e9962625d18eef9360bfa94bba8d49661f7
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2022-04-05 11:31:25 +0200

    📞 - Update Ajax Method

--- src/main/resources/static/js/index.script.js
+++ src/main/resources/static/js/index.script.js
@@ -1,15 +1,14 @@
 // Create Fetch API request
 var url = 'http://localhost:8080/api/users';
-fetch(url)
-    .then(function (response) {
-    return response.json();
-})
-    .then(function (data) {
-    console.log(data);
-    // Create a new table row for each user
-    for (var i = 0; i < data.length; i++) {
-        var user = data[i];
-        $("table").append("\n                    <tr>\n                        <td>".concat(user.firstName, "</td>\n                        <td>").concat(user.lastName, "</td>\n                    </tr>\n                "));
+// Create XMLHttpRequest request GET
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        for (var i = 0; i < JSON.parse(xhr.responseText).length; i++) {
+            var user = JSON.parse(xhr.responseText)[i];
+            document.querySelector("table").innerHTML += "\n                    <tr>\n                        <td>".concat(user.firstName, "</td>\n                        <td>").concat(user.lastName, "</td>\n                    </tr>\n                ");
+        }
     }
-    $("script").remove();
-});
+};
+xhr.open('GET', url, true);
+xhr.send();
