2ef0ed13a617a9e6765e5688f07772c2dbe6cd90
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-04-06 16:48:58 +0200

    add login

---
+++ src/main/resources/static/js/login.script.ts
@@ -0,0 +1,81 @@
+// On submit click
+document.querySelector('#submit').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    console.log("tesst");
+
+    const email:string = (<HTMLInputElement>document.querySelector('#email')).value; // Get the email value
+    const password:string = (<HTMLInputElement>document.querySelector('#password')).value; // Get the password value
+
+    const data = new FormData();
+    data.append("email", email);
+    data.append("password", password);
+
+    const xhr:XMLHttpRequest = new XMLHttpRequest();
+    if ( password && email ) {
+        // Register the account into the api
+        const url: string = 'http://localhost:8080/api/login';
+
+        xhr.open('POST', url, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                // If response is true, redirect to login page
+                if (xhr.responseText === 'true') {
+                    window.location.href = '/';
+                } else {
+                    // If response is false, show error message
+                    alert('Account doesn\'t exists');
+                }
+            }
+        };
+        xhr.send(data);
+    } else if ( !password || !email ) {
+        document.querySelector('#popUpContainer').innerHTML +=
+            '<div class="popup">' +
+            '<p class="popupMessage">Please fill in all the fields</p>' +
+            '<button class="popupButton">OK</button>' +
+            '</div>';
+
+        let popups = document.querySelectorAll('.popup');
+        for (let y = 0;  y < popups.length; y++) {
+            if (popups[y].classList.contains('coming')) {
+                popups[y].classList.remove('coming');
+            }
+        }
+        for (let i = popups.length-1; i < popups.length; i++) {
+            popups[i].classList.add('coming');
+        }
+
+
+
+        setTimeout(function () {
+            let popups = document.querySelectorAll('.popup')
+            for (let i = 0; i < popups.length; i++) {
+                if (!popups[i].classList.contains('leaving')) {
+                    popups[i].classList.add('leaving');
+                    break;
+                }
+            }
+
+
+
+
+        } , 1000);
+
+        // After 1.5 seconds the popup will disappear
+        setTimeout(function () {
+            document.querySelector('.popup').remove();
+        } , 1500);
+
+
+    }
+
+});
+
+document.addEventListener('click', (e) => {
+    //@ts-ignore
+    if (e.target.classList.contains('popupButton')) {
+        //@ts-ignore
+        e.target.parentNode.remove();
+    }
+});
