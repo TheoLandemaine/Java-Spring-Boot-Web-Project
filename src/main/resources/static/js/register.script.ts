a8686cf428b05d0902fd28ae8301df3c9ce6543f
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-06 11:54:55 +0200

    Create a register function

--- src/main/resources/static/js/register.script.ts
+++ src/main/resources/static/js/register.script.ts
@@ -2,34 +2,60 @@
 var url:string = 'http://localhost:8080/api/register';
 
 
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        var users = JSON.parse(xhr.responseText);
-        var user = {
-            // @ts-ignore
-            username: document.getElementById('username').value,
-            // @ts-ignore
-            email: document.getElementById('email').value,
-            // @ts-ignore
-            password: document.getElementById('password').value
-        };
-        users.push(user);
-        var xhr_1 = new XMLHttpRequest();
-        xhr_1.onreadystatechange = function() {
-            if (xhr_1.readyState === 4 && xhr_1.status === 200) {
-                console.log(xhr_1.responseText);
+
+document.querySelector('#submit').addEventListener('click', function(e) {
+    e.preventDefault();
+    var username:string = (<HTMLInputElement>document.querySelector('#username')).value;
+    var password:string = (<HTMLInputElement>document.querySelector('#password')).value;
+    var confirmPassword:string = (<HTMLInputElement>document.querySelector('#confirmPassword')).value;
+    var email:string = (<HTMLInputElement>document.querySelector('#email')).value;
+    var data:string = 'username=' + username + '&password=' + password + '&confirmPassword' + confirmPassword + '&email=' + email;
+    var xhr:XMLHttpRequest = new XMLHttpRequest();
+    if (username && password && email && confirmPassword && (password === confirmPassword)) {
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                var json: any = JSON.parse(xhr.responseText);
+                if (json.success) {
+                    window.location.href = 'http://localhost:8080/login';
+                } else {
+                    alert(json.message);
+                }
             }
         };
-        // @ts-ignore
-        if (document.querySelector('#password').value === document.querySelector('#confirm-password').value) {
-            xhr_1.open('POST', url, true);
-            xhr_1.setRequestHeader('Content-Type', 'application/json');
-            xhr_1.send(JSON.stringify(user));
-        } else {
-            alert('Password does not match');
-        }
+        xhr.send(data);
+    } else if ( !username || !password || !email || !confirmPassword ) {
+        document.querySelector('#popUpContainer').innerHTML +=
+            '<div class="popup">' +
+            '<p class="popupMessage">Please fill in all the fields</p>' +
+            '<button class="popupButton">OK</button>' +
+            '</div>';
+
+        // After 1.5 seconds the popup will disappear 
+        setTimeout(function () {
+            document.querySelector('.popup').remove();
+        } , 1500);
+
+
+    } else {
+
+        document.querySelector('#popUpContainer').innerHTML +=
+            '<div class="popup">' +
+            '<p class="popupMessage">Passwords do not match</p>' +
+            '<button class="popupButton">OK</button>' +
+            '</div>';
+
+            setTimeout(function () {
+                document.querySelector('.popup').remove();
+            } , 1500);
     }
-}
-xhr.open('POST', url, true);
-xhr.send();
+});
+
+document.addEventListener('click', (e) => {
+    //@ts-ignore
+    if (e.target.classList.contains('popupButton')) {
+        //@ts-ignore
+        e.target.parentNode.remove();
+    }
+});
