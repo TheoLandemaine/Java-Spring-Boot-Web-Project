7770ec6283590f879416289947caa584c79141b6
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-07 15:23:30 +0200

    Update login.script.ts

--- src/main/resources/static/js/login.script.ts
+++ src/main/resources/static/js/login.script.ts
@@ -22,7 +22,7 @@ document.querySelector('#submit').addEventListener('click', function(e) {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 // If response is true, redirect to login page
 
-                if (xhr.responseText) {
+                if (xhr.responseText !== "false") {
                     document.cookie = 'token=' + xhr.responseText;
 
                     window.location.href = '/';
