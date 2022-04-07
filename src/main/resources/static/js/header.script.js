462260f44c045c81ede288ea602d8e057cdcd386
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-07 13:22:09 +0200

    Update header to add a logout function and to show "Profile" and "Logout" if the user is connected

--- src/main/resources/static/js/header.script.js
+++ src/main/resources/static/js/header.script.js
@@ -23,8 +23,20 @@ if (checkCookie()) {
     console.log('user is logged in , is token : ', checkCookie());
     // @ts-ignore
     $('.navColumn').empty();
+    console.log($('.navColumn'));
     // @ts-ignore
-    $('.navColumn').append("\n        <li> <a href=\"/\">Profile </a> </li>\n        <li> <a href=\"/\">Logout </a> </li>\n       ");
+    $('.navColumn').append("\n        <li> <a href=\"/profile\">Profile </a> </li>\n        <li> <a href=\"/api/logout\">Logout </a> </li>\n       ");
+    // When user log out, delete token from the cookie
+    // @ts-ignore
+    $('.navColumn').on('click', function (e) {
+        // @ts-ignore
+        if (e.target.innerText === 'Logout') {
+            // @ts-ignore
+            document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+            // @ts-ignore
+            window.location.href = '/';
+        }
+    });
 }
 else {
     // @ts-ignore
--- src/main/resources/static/js/header.script.ts
+++ src/main/resources/static/js/header.script.ts
@@ -25,11 +25,25 @@ if (checkCookie()) {
     console.log('user is logged in , is token : ', checkCookie());
     // @ts-ignore
     $('.navColumn').empty();
+
+    console.log($('.navColumn'));
     // @ts-ignore
     $('.navColumn').append(`
-        <li> <a href="/">Profile </a> </li>
-        <li> <a href="/">Logout </a> </li>
+        <li> <a href="/profile">Profile </a> </li>
+        <li> <a href="/api/logout">Logout </a> </li>
        `);
+
+    // When user log out, delete token from the cookie
+    // @ts-ignore
+    $('.navColumn').on('click', (e) => {
+        // @ts-ignore
+        if (e.target.innerText === 'Logout') {
+            // @ts-ignore
+            document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+            // @ts-ignore
+            window.location.href = '/';
+        }
+    })
 } else {
     // @ts-ignore
     console.log('user is not logged in , is token : ', checkCookie());
