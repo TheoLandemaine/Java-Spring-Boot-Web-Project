86958e12440f45dac931fb8a55748461b18dd9bd
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-11 15:37:56 +0200

    Create CardService.java

---
+++ src/main/java/com/codingfactory/porare/service/CardService.java
@@ -0,0 +1,37 @@
+package com.codingfactory.porare.service;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CardService {
+    private JdbcTemplate jdbcTemplate;
+
+    public CardService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // add card
+    public void addCard(String token, int cardId) {
+        DecodedJWT jwt = JWT.decode(token);
+
+        String sql = "INSERT INTO card (u_id, c_id) VALUES (?, ?)";
+        jdbcTemplate.update(sql, jwt.getClaim("userId"), cardId);
+
+    }
+
+    // delete card
+    public void deleteCard(int c_id, String token) {
+        DecodedJWT jwt = JWT.decode(token);
+
+        String sql = "DELETE FROM card WHERE u_id = ? AND c_id = ? LIMIT 1";
+        jdbcTemplate.update(sql, jwt.getClaim("userId"), c_id);
+    }
+
+
+}
