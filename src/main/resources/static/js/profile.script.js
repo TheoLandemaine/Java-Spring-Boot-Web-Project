a86fcfd37fa4ce2a608f8f84739ef41304359a9d
ilan <ilan.petiot@icloud.com>
2022-04-12 20:23:55 +0200

    🔁 - Change js into jquery

--- src/main/resources/static/js/profile.script.js
+++ src/main/resources/static/js/profile.script.js
@@ -1,26 +1,16 @@
-// @ts-ignore
-$('#logout').on('click', function () {
-    document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-    window.location.href = '/';
-});
-var edit = document.querySelector('#edit');
-var modifyPassword = document.querySelector('#modifyPassword');
-var supp = document.querySelector('#delete');
-var myPacks = document.querySelector('#myPacks');
-var logout = document.querySelector('#logout');
-edit.addEventListener("click", function (e) {
+$('#edit').on('click', function () {
     window.location.href = '/edit';
-    console.log("test");
 });
-modifyPassword.addEventListener("click", function (e) {
-    window.location.href = '/password';
+$('#modifyPassword').on('click', function () {
+    window.location.href = '/modifyPassword';
 });
-supp.addEventListener("click", function (e) {
+$('#delete').on('click', function () {
     window.location.href = '/delete';
 });
-myPacks.addEventListener("click", function (e) {
+$('#myPacks').on('click', function () {
     window.location.href = '/myPacks';
 });
-logout.addEventListener("click", function (e) {
+$('#logout').on('click', function () {
+    document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
     window.location.href = '/';
 });
--- src/main/resources/static/js/profile.script.ts
+++ src/main/resources/static/js/profile.script.ts
@@ -1,33 +1,20 @@
-// @ts-ignore
-$('#logout').on('click', () => {
-        document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-        window.location.href = '/';
-
+$('#edit').on('click', () => {
+    window.location.href = '/edit';
 })
 
-let edit = document.querySelector('#edit');
-let modifyPassword = document.querySelector('#modifyPassword');
-let supp = document.querySelector('#delete');
-let myPacks = document.querySelector('#myPacks');
-let logout = document.querySelector('#logout');
-
-edit.addEventListener("click", function (e) {
-        window.location.href = '/edit';
-        console.log("test");
+$('#modifyPassword').on('click', () => {
+        window.location.href = '/modifyPassword';
 })
 
-modifyPassword.addEventListener("click", function (e) {
-        window.location.href = '/password';
-})
-
-supp.addEventListener("click", function (e) {
+$('#delete').on('click', () => {
         window.location.href = '/delete';
 })
 
-myPacks.addEventListener("click", function (e) {
+$('#myPacks').on('click', () => {
         window.location.href = '/myPacks';
 })
 
-logout.addEventListener("click", function (e) {
+$('#logout').on('click', () => {
+        document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
         window.location.href = '/';
 })
