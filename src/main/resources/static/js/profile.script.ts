a8cd594e28998af1f24d05d64ab7054a84bf9f56
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-04-12 16:28:38 +0200

    update profil js

--- src/main/resources/static/js/profile.script.ts
+++ src/main/resources/static/js/profile.script.ts
@@ -1,4 +1,30 @@
+// @ts-ignore
 $('#logout').on('click', () => {
+
+        // @ts-ignore
         document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        // @ts-ignore
+        window.location.href = '/';
+
+})
+
+var edit = document.querySelector('#edit');
+var modifyPassword = document.querySelector('#modifyPassword');
+var supp = document.querySelector('#delete');
+var logout = document.querySelector('#logout');
+
+edit.addEventListener("click", function (e) {
+        window.location.href = '/edit';
+})
+
+modifyPassword.addEventListener("click", function (e) {
+        window.location.href = '/password';
+})
+
+supp.addEventListener("click", function (e) {
+        window.location.href = '/delete';
+})
+
+logout.addEventListener("click", function (e) {
         window.location.href = '/';
 })
