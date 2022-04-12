92ca0aa673ae7c4fcc6432f7026fd0506b7f547d
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-12 18:06:04 +0200

    Correcting a bug and make "supp" invisible for now

--- src/main/resources/static/js/profile.script.js
+++ src/main/resources/static/js/profile.script.js
@@ -6,15 +6,25 @@ var edit = document.querySelector('#edit');
 var modifyPassword = document.querySelector('#modifyPassword');
 var supp = document.querySelector('#delete');
 var logout = document.querySelector('#logout');
+var myPacks = document.querySelector('#myPacks');
+var myCards = document.querySelector('#myCards');
 edit.addEventListener("click", function (e) {
     window.location.href = '/edit';
 });
 modifyPassword.addEventListener("click", function (e) {
     window.location.href = '/password';
 });
+/*
 supp.addEventListener("click", function (e) {
-    window.location.href = '/delete';
-});
+        window.location.href = '/delete';
+})
+*/
 logout.addEventListener("click", function (e) {
     window.location.href = '/';
+});
+myPacks.addEventListener("click", function (e) {
+    window.location.href = '/myPacks';
+});
+myCards.addEventListener("click", function (e) {
+    window.location.href = '/myCards';
 });
--- src/main/resources/static/js/profile.script.ts
+++ src/main/resources/static/js/profile.script.ts
@@ -4,10 +4,12 @@ $('#logout').on('click', () => {
 
 })
 
-var edit = document.querySelector('#edit');
-var modifyPassword = document.querySelector('#modifyPassword');
-var supp = document.querySelector('#delete');
-var logout = document.querySelector('#logout');
+const edit = document.querySelector('#edit');
+const modifyPassword = document.querySelector('#modifyPassword');
+const supp = document.querySelector('#delete');
+const logout = document.querySelector('#logout');
+const myPacks = document.querySelector('#myPacks');
+const myCards = document.querySelector('#myCards');
 
 edit.addEventListener("click", function (e) {
         window.location.href = '/edit';
@@ -17,10 +19,20 @@ modifyPassword.addEventListener("click", function (e) {
         window.location.href = '/password';
 })
 
+/*
 supp.addEventListener("click", function (e) {
         window.location.href = '/delete';
 })
+*/
 
 logout.addEventListener("click", function (e) {
         window.location.href = '/';
+})
+
+myPacks.addEventListener("click", function (e) {
+        window.location.href = '/myPacks';
+})
+
+myCards.addEventListener("click", function (e) {
+        window.location.href = '/myCards';
 })
