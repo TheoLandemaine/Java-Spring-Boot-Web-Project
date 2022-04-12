5ecb7b785d13778e492336a71cecfdc0160b49e8
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2022-04-12 16:54:32 +0200

    🗑 - Delete fuck**g @ts-ignore

--- src/main/resources/static/js/profile.script.ts
+++ src/main/resources/static/js/profile.script.ts
@@ -1,9 +1,6 @@
 // @ts-ignore
 $('#logout').on('click', () => {
-
-        // @ts-ignore
         document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-        // @ts-ignore
         window.location.href = '/';
 
 })
