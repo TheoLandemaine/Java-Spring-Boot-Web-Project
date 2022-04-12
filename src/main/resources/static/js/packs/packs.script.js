783710dc1451fdeb40654b73d238d8662280bda0
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-12 21:42:14 +0200

    Functions to generate pack from our API and deletePack when a pack is opened

--- src/main/resources/static/js/packs/packs.script.js
+++ src/main/resources/static/js/packs/packs.script.js
@@ -40,42 +40,56 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 $(document).ready(function () {
     // drawRandomPokemons();
     //generatePacksFromAPI();
-    generatePacksArtificially();
+    // @ts-ignore
+    generatePacksFromAPI(checkCookie());
 });
 function generatePacksFromAPI(token) {
     // Create Fetch API request
     // @ts-ignore
-    var url = 'http://localhost:8080/api/getPacks';
+    var url = '/api/getPacks/';
     // Create XMLHttpRequest request GET
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            // Clear div
-            var div = document.querySelector(".allPacks")[0];
-            div.innerHTML = '';
-            for (var i = 0; i < JSON.parse(xhr.responseText).length; i++) {
-                var pack = JSON.parse(xhr.responseText)[i];
-                div.innerHTML += "\n                    <div class = \"pack\" data-attr=\"".concat(pack.p_type, "\">\n                        <div class = \"packFace\">\n                        <img  class=\"openPack\" src=\"").concat(packVisual(pack.p_type), "\">\n                    </div> \n                    </tr>\n                ");
+    var data = {
+        // @ts-ignore
+        token: token
+    };
+    $.post(url, data, function (response) {
+        if (response !== false) {
+            for (var i = 0; i < response.length; i++) {
+                var pack = response[i];
+                $('.allPacks').append("\n                    <div class = \"pack\" data-attr=\"".concat(pack, "\">\n                        <div class = \"packFace\">\n                        <img  class=\"openPack\" src=\"").concat(packVisual(pack), "\">\n                    </div> \n                    </tr>\n                "));
             }
         }
-    };
-    xhr.open('GET', url, true);
-    xhr.send();
+        else {
+            alert('Vous n\'avez pas de pack, veuillez en acheter');
+            window.location.href = '/shop';
+        }
+    });
 }
+/*
 function generatePacksArtificially() {
-    for (var i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
+
         generatePacks('colorless');
         generatePacks('fire');
         generatePacks('grass');
         generatePacks('random');
+
     }
+
+
+
+
+
     //
-}
+}*/
 $(document).click(function (e) {
     // @ts-ignore
     if (e.target.classList.contains('openPack')) {
         console.log("azertyuiop");
+        // @ts-ignore
+        deletePackFromDB(e.target.parentNode.parentNode.getAttribute('data-attr'));
         clearPacks();
+        // @ts-ignore
         animationBoosters(e.target.parentNode.parentNode.getAttribute('data-attr'));
         // @ts-ignore
         drawPokemons(e.target.parentNode.parentNode.getAttribute('data-attr'));
@@ -101,8 +115,10 @@ $(document).mouseover(function (e) {
 function animationBoosters(packType) {
     console.log("animationBoosters");
     var div = document.querySelector('.allPacks');
+    // @ts-ignore
     div.style.height = "100vh";
     $('.allPacks').append("\n                    <div id=\"pack-opened\" class=\"col-xs-12 open\">\n                        <div class=\"pack-content\" style=\"display: block !important; visibility: visible !important;\" >\n                            <div class=\"pack-flash\">\n                                <div class=\"pack-flash-pack\" >\n                                    <img class=\"front\" src=\"" + packVisual(packType) + " \" > \n                                    <div class=\"top\">\n                                        <img src=\"https://i.imgur.com/b1qmOW6.png\">\n                                        <div class=\"cut\"> \n                                            <img src=\"https://i.imgur.com/k55nnYY.png\">\n                                        </div>\n                                        <span> \n                                            <img src=\"https://i.imgur.com/JqedAsJ.png\">\n                                               <span>\n                                                <img src=\"https://i.imgur.com/WWRXjri.png\">\n                                                    <span>\n                                                        <img src=\"https://i.imgur.com/DzEYvSP.png\" style=\"width: 81px\"> \n                                                    </span>\n                                                </span>\n                                            </span>\n                                        </div>\n                                    </div>\n                                </div>\n                            </div>\n                        </div>\n                    </div>");
+    // @ts-ignore
     setTimeout(function () { div.style.height = null; }, 2700);
     setTimeout(clearPacks, 2700);
 }
@@ -212,7 +228,8 @@ $(document).click(function (e) {
 $('.returnToPacks').click(function () {
     clearCards();
     // Go to the top of the page
-    generatePacksArtificially();
+    // @ts-ignore
+    generatePacksFromAPI(checkCookie());
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
     $('.returnToPacks').css('display', 'none');
@@ -296,25 +313,25 @@ function packVisual(packType) {
     return "./img/packart/".concat(packType, "/").concat(randomImage, ".jpg");
 }
 function deletePackFromDB(packType) {
-    var token = localStorage.getItem('token');
+    console.log('delete this pack');
+    // @ts-ignore
+    var token = checkCookie();
     var url = 'http://localhost:8080/api/deletePack';
-    var xhr = new XMLHttpRequest();
-    var data = new FormData();
-    data.append('packType', packType);
-    data.append('token', token);
-    xhr.open('POST', url, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
+    var data = {
+        token: token,
+        packType: packType
+    };
+    $.post(url, data, function (response) {
+        if (response == 4 && response == 200) {
             // If response is true, redirect to login page
-            if (xhr.responseText !== 'false') {
+            if (response !== 'false') {
             }
             else {
                 // If response is false, show error message
-                alert('Username already exists');
+                alert("This pack doesn't exist in this databse");
             }
         }
-    };
-    xhr.send(data);
+    });
 }
 function saveCards(cards) {
     // Get the token of the user
