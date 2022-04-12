cf870e2aa5d6ab0082c0b9ba01e9152501bb4b8d
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-13 00:27:02 +0200

    Create a cards script to print cards from the db

---
+++ src/main/resources/static/js/cards.script.ts
@@ -0,0 +1,52 @@
+$(document).ready(() => {
+    //@ts-ignore
+    generateCardsFromAPI(checkCookie());
+});
+
+function generateCardsFromAPI(token) {
+    // Create Fetch API request
+// @ts-ignore
+
+
+
+
+
+
+
+        const url: string = '/api/getCards';
+        // Create XMLHttpRequest request GET
+        const data: Object = {
+            // @ts-ignore
+            token: token
+        };
+
+
+        $.post(url, data, (response) => {
+
+
+            if (response !== false) {
+
+                for (let i: number = 0; i < response.length; i++) {
+                    let cardId: any = response[i];
+                    let urlAPI = 'https://api.pokemontcg.io/v2/cards/?q=id:' + cardId;
+                    $.get(urlAPI, (response2) => {
+                    let card:any = response2;
+
+
+
+                    $('.allCards').append(`
+                    <div class = "card" data-attr="${card}">
+                    <img src="${response2.data[0].images['large']}" alt="${card.name}">
+                    </div>
+                    `);
+                    });
+                }
+
+            } else {
+                alert('Vous n\'avez pas de pack, veuillez en acheter');
+                window.location.href = '/shop';
+            }
+        });
+
+
+}
