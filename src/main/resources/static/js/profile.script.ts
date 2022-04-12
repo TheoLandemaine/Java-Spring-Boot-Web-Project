c95ca746f3115358bd18c2973a252e705d5bb2a9
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-04-12 10:43:26 +0200

    change css button profile

--- src/main/resources/static/js/profile.script.ts
+++ src/main/resources/static/js/profile.script.ts
@@ -8,3 +8,24 @@ $('#logout').on('click', () => {
         window.location.href = '/';
 
 })
+
+var edit = document.querySelector('#edit');
+var modifyPassword = document.querySelector('#modifyPassword');
+var supp = document.querySelector('#delete');
+var logout = document.querySelector('#logout');
+
+edit.addEventListener("click", function (e) {
+        window.location.href = '/edit';
+})
+
+modifyPassword.addEventListener("click", function (e) {
+        window.location.href = '/password';
+})
+
+supp.addEventListener("click", function (e) {
+        window.location.href = '/delete';
+})
+
+logout.addEventListener("click", function (e) {
+        window.location.href = '/';
+})
