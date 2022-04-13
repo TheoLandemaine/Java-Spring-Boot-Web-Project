d9ee19fa4da02e1db91eccfd0a383f9511187a03
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-13 23:02:12 +0200

    Append the gifts

--- src/main/resources/static/js/header.script.js
+++ src/main/resources/static/js/header.script.js
@@ -11,15 +11,27 @@ document.addEventListener('mouseover', function (e) {
     }
 });
 // @ts-ignore
+$.post('api/getGiftAccess', { 'token': checkCookie() }, function (data) {
+    console.log(data);
+    if (data) {
+        // @ts-ignore
+        $.post('/api/randomGift', { 'token': checkCookie() }, function (data) {
+            {
+                $('.navColumn').append("<li><a class=\"nav applyGift\"><span class=\"giftValue\">".concat(data, "</span> <i class=\"fas fa-hand-holding-usd\"></i></a></li>"));
+            }
+        });
+    }
+});
+// @ts-ignore
 $.post('/api/getUserCoins', { 'token': checkCookie() }, function (data) {
-    $('.navColumn').append("<li><a href=\"./shop\">".concat(data, " <i class=\"fas fa-coins\"></i></a></li>"));
+    $('.navColumn').append("<li><a class=\"nav\" href=\"./shop\">".concat(data, " <i class=\"fas fa-coins\"></i></a></li>"));
 });
 // @ts-ignore
 if (checkCookie() !== 'false') {
     $('.navColumn').empty();
     $('.firstNav').empty();
-    $('.navColumn').append("<li><a href=\"/profile\">Profile</a></li>");
-    $('.firstNav').append("\n        <li><a href=\"./\">Home</a></li>\n<!--        <li><a href=\"./users\">Users</a></li>-->\n        <li><a href=\"./search\">Catalog</a></li>\n        <li><a href=\"./shop\">Shop</a></li>\n    ");
+    $('.navColumn').append("<li><a class=\"nav\" href=\"/profile\">Profile</a></li>");
+    $('.firstNav').append("\n        <li><a class=\"nav\" href=\"./\">Home</a></li>\n<!--        <li><a href=\"./users\">Users</a></li>-->\n        <li><a class=\"nav\" href=\"./search\">Catalog</a></li>\n        <li><a class=\"nav\" href=\"./shop\">Shop</a></li>\n    ");
 }
 // @ts-ignore
 $.post('/api/getCards', { 'token': checkCookie() }, function (data) {
@@ -30,6 +42,34 @@ $.post('/api/getCards', { 'token': checkCookie() }, function (data) {
 // @ts-ignore
 $.post('/api/getPacks', { 'token': checkCookie() }, function (data) {
     if (data.length > 0) {
-        $('.firstNav').append("<li><a href=\"./myPacks\">My Packs</a></li>");
+        $('.firstNav').append("<li><a class=\"nav\" href=\"./myPacks\">My Packs</a></li>");
+    }
+});
+$(document).click(function (e) {
+    var target = e.target;
+    if (target.classList.contains('applyGift') || target.parentNode.classList.contains('applyGift')) {
+        // Get the value of .giftValue
+        var giftValue = $('.giftValue').text();
+        console.log(giftValue);
+        $('.navColumn').empty();
+        // @ts-ignore
+        $.post('api/giveGift', { 'token': checkCookie(), 'gift': parseInt(giftValue) }, function (response) {
+            if (response !== false) {
+            }
+            else {
+                alert('You have no coins to clear');
+            }
+            $('.navColumn').append("<li><a class=\"nav\" href=\"./profile\">Profile</a></li>");
+            // @ts-ignore
+            $.post('/api/getUserCoins', { 'token': checkCookie() }, function (data) {
+                $('.navColumn').append("<li><a class=\"nav\" href=\"./shop\">".concat(data, " <i class=\"fas fa-coins\"></i></a></li>"));
+            });
+        });
+    }
+});
+$(document).mouseover(function (e) {
+    var target = e.target;
+    if (target.classList.contains('applyGift')) {
+        target.style.cursor = 'pointer';
     }
 });
