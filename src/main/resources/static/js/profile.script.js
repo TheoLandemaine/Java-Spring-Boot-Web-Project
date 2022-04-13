bf2ae9764512b839892bb515c79e3ada925ab225
ilan <ilan.petiot@icloud.com>
2022-04-13 11:38:24 +0200

    test

--- src/main/resources/static/js/profile.script.js
+++ src/main/resources/static/js/profile.script.js
@@ -14,3 +14,13 @@ $('#logout').on('click', function () {
     document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
     window.location.href = '/';
 });
+$.get('https://api.pokemontcg.io/v2/cards?q=id:swsh45sv-SV107', function (data) {
+    var card = data.data[0];
+    console.log(data);
+    console.log(data.data);
+    console.log(data.data[0].id);
+    console.log(data.data[0].images);
+    console.log(data.data[0].images.small);
+    $('#myCards').append("<div> ".concat(card.id, " ").concat(card.name, "</div>"));
+    $('#myCards').append("<img src=\"".concat(card.images.small, "\" class=\"resultsImage\" alt=\"your pokemon card\">"));
+});
--- src/main/resources/static/js/profile.script.ts
+++ src/main/resources/static/js/profile.script.ts
@@ -18,3 +18,16 @@ $('#logout').on('click', () => {
         document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
         window.location.href = '/';
 })
+
+$.get('https://api.pokemontcg.io/v2/cards?q=id:swsh45sv-SV107', (data)=> {
+    const card = data.data[0];
+    console.log(data);
+    console.log(data.data)
+    console.log(data.data[0].id);
+    console.log(data.data[0].images);
+    console.log(data.data[0].images.small);
+
+    $('#myCards').append(`<div> ${card.id} ${card.name}</div>`);
+    $('#myCards').append(`<img src="${card.images.small}" class="resultsImage" alt="your pokemon card">`);
+
+});
