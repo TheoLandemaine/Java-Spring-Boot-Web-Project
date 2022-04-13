542dfbfc587e6cf6bc71d4263201f3c31e6fdcb2
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-04-13 15:00:32 +0200

    add cards profile

--- src/main/resources/static/js/profile.script.js
+++ src/main/resources/static/js/profile.script.js
@@ -18,3 +18,33 @@ var myCards = document.querySelector('#myCards');
 myCards.addEventListener("click", function (e) {
     window.location.href = '/myCards';
 });
+$(document).ready(function () {
+    //@ts-ignore
+    generateCardsFromAPI(checkCookie());
+});
+function generateCardsFromAPI(token) {
+    // Create Fetch API request
+    // @ts-ignore
+    var url = '/api/getCards';
+    // Create XMLHttpRequest request GET
+    var data = {
+        // @ts-ignore
+        token: token
+    };
+    $.post(url, data, function (response) {
+        if (response !== false) {
+            for (var i = 0; i < response.length; i++) {
+                var cardId = response[i];
+                var urlAPI = 'https://api.pokemontcg.io/v2/cards/?q=id:' + cardId;
+                $.get(urlAPI, function (response2) {
+                    var card = response2;
+                    $('.allCards').append("\n                    <div class = \"card\" data-attr=\"".concat(card, "\">\n                    <img src=\"").concat(response2.data[0].images['small'], "\" alt=\"").concat(card.name, "\">\n                    </div>\n                    "));
+                });
+            }
+        }
+        else {
+            alert('Vous n\'avez pas de pack, veuillez en acheter');
+            window.location.href = '/shop';
+        }
+    });
+}
